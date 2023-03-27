3185842b5c1e31747e654343718a4343eaa8fc86
hasantezcan <hasantezcan77@gmail.com>
2023-03-27 20:36:28 +0300

    feat: add storybook deploy workflow

--- build/AsciinemaPlayer.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AsciiOptions } from "./types";
-interface AsciinemaPlayerProps extends React.HTMLAttributes<HTMLDivElement> {
-    src: string;
-    options?: AsciiOptions;
-}
-declare const AsciinemaPlayer: React.ForwardRefExoticComponent<AsciinemaPlayerProps & React.RefAttributes<HTMLDivElement>>;
-export default AsciinemaPlayer;
